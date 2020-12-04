aceea5d4e369fc02af0785be58de5518061160c1
training <training@training.epics>
2020-12-04 09:24:19 -0500

    Added directory creation for log file, put string messages in variables

--- procServUtils/log.py
+++ procServUtils/log.py
@@ -3,7 +3,9 @@ from .conf import getrundir
 tail  = '/usr/bin/tail'
 less  = '/usr/bin/less'
 reset = '/usr/bin/reset'
-
+instance_not_found = "Instance '%s' not found"
+loggin_not_configured = "Logging is not configured for '%s', check configuration file\n"
+log_file_does_not_exist = "Logfile '%s' does not exist yet, do you want to create it?"
 def logs(conf, args):
     name = args.name
     logfile_path = log_file_path(name, conf)
@@ -18,13 +20,12 @@ def logs(conf, args):
             subprocess.run(reset)
 
 def log_file_path(name, conf):
-    if not conf.has_section(name):
-        print_err_and_exit("Instance '%s' not found"%name, 1)
-    if not conf.has_option(name, 'logfile') or conf.get(name, 'logfile') == '-':
-        print_err_and_exit("Logging is not configured for '%s', check configuration file\n"%name, 1)
+    if not conf.has_section(name): print_err_and_exit(instance_not_found % name, 1)
+    if not conf.has_option(name, 'logfile') or conf.get(name, 'logfile') == '-': print_err_and_exit(loggin_not_configured % name, 1)
     logfile_path = conf.get(name, 'logfile') if os.path.isabs(conf.get(name, 'logfile')) else '/'.join([conf.get(name,'chdir'),conf.get(name, 'logfile')])
     if not os.path.isfile(logfile_path):
-        if yes_or_no("Logfile '%s' does not exist yet, do you want to create it?"%logfile_path):
+        if yes_or_no(log_file_does_not_exist % logfile_path):
+            os.makedirs(os.path.split(logfile_path)[0], exist_ok=True)
             open(logfile_path, 'w').close()
         else:
             print_err_and_exit("then there will be no logs\n",1)
