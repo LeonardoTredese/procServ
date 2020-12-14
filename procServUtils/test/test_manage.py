420f28efc77027f128dd099c25473734986cef0d
Leonardo Tredese <leonardo.tredese@lnl.infn.it>
2020-12-14 15:22:09 +0100

    added som unit tests

--- procServUtils/test/test_manage.py
+++ procServUtils/test/test_manage.py
@@ -8,42 +8,145 @@ from . import TestDir
 manage.systemctl = '/bin/true'
 
 class TestGen(unittest.TestCase):
-    def test_add(self):
+    def test_add_basic(self):
         with TestDir() as t:
-            main(getargs(['add', '-C', '/somedir', 'instname', '--', '/bin/sh', '-c', 'blah']), test=True)
+            main(getargs(['add', '-C', '/somedir', 'instance-0', '--', '/bin/sh', '-c', 'blah']), test=True)
             #os.system('find '+t.dir)
 
-            confname = t.dir+'/procServ.d/instname.conf'
+            confname = t.dir+'/procServ.d/instance-0.conf'
 
             self.assertTrue(os.path.isfile(confname))
             with open(confname, 'r') as F:
                 content = F.read()
 
             self.assertEqual(content, """
-[instname]
+[instance-0]
 command = /bin/sh -c blah
 chdir = /somedir
 """)
-
+    def test_add_user(self):
+        with TestDir() as t:
             main(getargs(['add',
                           '-C', '/somedir',
                           '-U', 'someone',
-                          '-G', 'controls',
-                          'other', '--', '/bin/sh', '-c', 'blah']), test=True)
+                          'instance-1', '--', '/bin/sh', '-c', 'blah']), test=True)
 
-            confname = t.dir+'/procServ.d/other.conf'
+            confname = t.dir+'/procServ.d/instance-1.conf'
 
             self.assertTrue(os.path.isfile(confname))
             with open(confname, 'r') as F:
                 content = F.read()
 
             self.assertEqual(content, """
-[other]
+[instance-1]
 command = /bin/sh -c blah
 chdir = /somedir
 user = someone
-group = controls
 """)
+    def test_add_group(self):
+        with TestDir() as t:
+            main(getargs(['add',
+                          '-C', '/somedir',
+                          '-G', 'somegroup',
+                          'instance-2', '--', '/bin/sh', '-c', 'blah']), test=True)
+
+            confname = t.dir+'/procServ.d/instance-2.conf'
+
+            self.assertTrue(os.path.isfile(confname))
+            with open(confname, 'r') as F:
+                content = F.read()
+
+            self.assertEqual(content, """
+[instance-2]
+command = /bin/sh -c blah
+chdir = /somedir
+group = somegroup
+""")
+
+    def test_add_port_tcp(self):
+        with TestDir() as t:
+            main(getargs(['add',
+                          '-C', '/somedir',
+                          '-P', 'tcp:6666',
+                          'instance-3', '--', '/bin/sh', '-c', 'blah']), test=True)
+
+            confname = t.dir+'/procServ.d/instance-3.conf'
+
+            self.assertTrue(os.path.isfile(confname))
+            with open(confname, 'r') as F:
+                content = F.read()
+
+            self.assertEqual(content, """
+[instance-3]
+command = /bin/sh -c blah
+chdir = /somedir
+port = tcp:6666
+""")
+
+    def test_add_port_unix(self):
+        with TestDir() as t:
+            main(getargs(['add',
+                          '-C', '/somedir',
+                          '-P', 'unix:test-port', 
+                          'instance-4', '--', '/bin/sh', '-c', 'blah']), test=True)
+
+            confname = t.dir+'/procServ.d/instance-4.conf'
+
+            self.assertTrue(os.path.isfile(confname))
+            with open(confname, 'r') as F:
+                content = F.read()
+
+            self.assertEqual(content, """
+[instance-4]
+command = /bin/sh -c blah
+chdir = /somedir
+port = unix:test-port
+""")
+
+    def test_add_basic_logfile_relpath(self):
+        with TestDir() as t:
+            logfile = 'log.txt'
+            chdir   = '/somedir'
+            main(getargs(['add',
+                          '-C', chdir,
+                          '-L', logfile,
+                          'instance-5', '--', '/bin/sh', '-c', 'blah']), test=True)
+            confname = t.dir+'/procServ.d/instance-5.conf'
+            self.assertTrue(os.path.isfile(confname))
+            with open(confname, 'r') as F:
+                self.assertEqual(F.read(), """
+[instance-5]
+command = /bin/sh -c blah
+chdir = /somedir
+logfile = log.txt
+""")
+            self.assertTrue(os.path.isfile(chdir + '/' + logfile))
+            os.remove(chdir + '/' + logfile)
+            os.rmdir(chdir)
+            self.assertFalse(os.path.isfile(chdir + '/' + logfile))
+            
+    def test_add_basic_logfile_abspath(self):
+        with TestDir() as t:
+            logfile = os.path.expanduser("~") + '/random/dir/log.txt'
+            chdir   = '/somedir'
+            main(getargs(['add',
+                          '-C', chdir,
+                          '-L', logfile,
+                          'instance-6', '--', '/bin/sh', '-c', 'blah']), test=True)
+            confname = t.dir+'/procServ.d/instance-6.conf'
+            self.assertTrue(os.path.isfile(confname))
+            with open(confname, 'r') as F:
+                self.assertEqual(F.read(), """
+[instance-6]
+command = /bin/sh -c blah
+chdir = /somedir
+""" + "logfile = " + logfile + "\n")
+
+            self.assertTrue(os.path.isfile(logfile))
+            os.remove(logfile)
+            os.rmdir(os.path.expanduser("~") + '/random/dir')
+            os.rmdir(os.path.expanduser("~") + '/random')
+            self.assertFalse(os.path.isfile(logfile))
 
     def test_remove(self):
         with TestDir() as t:
