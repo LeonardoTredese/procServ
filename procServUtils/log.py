bccd3e03d4c8fae846313ed7cd8fd9bc7f22642e
Leonardo Tredese <leonardo.tredese@lnl.infn.it>
2020-12-14 15:28:33 +0100

    refactored logfile access

--- procServUtils/log.py
+++ procServUtils/log.py
@@ -6,6 +6,7 @@ reset = '/usr/bin/reset'
 instance_not_found = "Instance '%s' not found"
 loggin_not_configured = "Logging is not configured for '%s', check configuration file\n"
 log_file_does_not_exist = "Logfile '%s' does not exist yet, do you want to create it?"
+
 def logs(conf, args):
     name = args.name
     logfile_path = log_file_path(name, conf)
@@ -19,14 +20,22 @@ def logs(conf, args):
             # reset terminal on reckless close
             subprocess.run(reset)
 
+def get_logfile_path(directory, logfile):
+    return logfile if os.path.isabs(logfile) else directory + '/' + logfile
+
+def create_logfile(abs_logfile_path): 
+    os.makedirs(os.path.split(abs_logfile_path)[0], exist_ok=True)
+    open(abs_logfile_path, 'w').close()
+
 def log_file_path(name, conf):
-    if not conf.has_section(name): print_err_and_exit(instance_not_found % name, 1)
-    if not conf.has_option(name, 'logfile') or conf.get(name, 'logfile') == '-': print_err_and_exit(loggin_not_configured % name, 1)
-    logfile_path = conf.get(name, 'logfile') if os.path.isabs(conf.get(name, 'logfile')) else '/'.join([conf.get(name,'chdir'),conf.get(name, 'logfile')])
+    if not conf.has_section(name):
+        print_err_and_exit(instance_not_found % name, 1)
+    if not conf.has_option(name, 'logfile') or conf.get(name, 'logfile') == '-':
+        print_err_and_exit(loggin_not_configured % name, 1)
+    logfile_path = get_logfile_path(conf.get(name, 'chdir'), conf.get(name, 'logfile'))
     if not os.path.isfile(logfile_path):
         if yes_or_no(log_file_does_not_exist % logfile_path):
-            os.makedirs(os.path.split(logfile_path)[0], exist_ok=True)
-            open(logfile_path, 'w').close()
+            create_logfile(logfile_path)
         else:
             print_err_and_exit("then there will be no logs\n",1)
     return logfile_path
